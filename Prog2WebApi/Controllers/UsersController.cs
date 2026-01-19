86757f3c009bc452feaece6d7c04857d57e92916
rebeka.pilscikova <rebeka.pilscikova@visma.com>
2026-01-19 13:08:13 +0200

    add users controller

---
+++ Prog2WebApi/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prog2WebApi.Data;
+using Prog2WebApi.Models;
+
+namespace Prog2WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        // šeit notiek AppDbContext injekcija
+        public UsersController(AppDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser()
+        {
+        }
+    }
+}
---
+++ Prog2WebApi/Models/Requests/UserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Prog2WebApi.Models.Requests
+{
+    public class UserRequest
+    {
+        string Username { get; set; }
+        string Password { get; set; }
+    }
+}
